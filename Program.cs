eddcbc1632c248e3fbc5387a0f60cac783c9c496
Nagibator-666 <nik.565565@gmail.com>
2026-03-19 11:34:10 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Diagnostics;
+
+foreach(Process process in Process.GetProcesses())
+    {
+        Console.WriteLine($"ID: {process.Id}");
+    }
+    
+Process process1 = Process.GetProcessesByName("devenv")[0];
+ProcessThreadCollection processThread = process1.Threads;
+
+foreach (ProcessThread thread in processThread)
+    {
+        Console.WriteLine($"ThreadId: {thread.Id}");
+    }
+
+Process.Start(@"C:\Users\23_ИП-291к\Desktop\DB Browser for SQLite.exe");
+Process.Start(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\");
